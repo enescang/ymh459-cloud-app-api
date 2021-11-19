09d0e531a812db8c7fc07f0891845f842c37709a
kodcanlisi <developerindiee@gmail.com>
2021-11-19 08:35:55 +0300

    MongoDB user scheme

---
+++ modules/user/models/user.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const { Types } = Schema;
+const user_schema = new Schema({
+    _id: { type: Types.String, required: true },
+    email: { type: Types.String, required: true },
+    password: { type: Types.String, required: true },
+    username: { type: Types.String, required: true },
+    language: { type: Types.String, default: "en" },
+}, {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
+
+module.exports = model(`users`, user_schema);
